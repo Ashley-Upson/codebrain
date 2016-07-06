48a1df8152d017e88080465e6b143275827adb79
Ashley Upson <ashmisterupson@gmail.com>
2016-07-06 07:04:29 +0100

    Added new code. Optimised the activity log.

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,32 @@
-function checkCommandInput() {
+
+
+function addActivity(command) {
+	"use strict";
+	var receivedCommand = command,
+		activityLogElement = document.getElementById("activityLog"),
+		currentActivities = activityLogElement.innerHTML,
+		newActivityLog = currentActivities + "<br>" + "<b>$USER_CONSOLE >>></b> " + receivedCommand;
+	activityLogElement.innerHTML = newActivityLog;
+	activityLogElement.scrollTop = activityLogElement.scrollHeight;
+}
+
+
+function sendCommand() {
+	"use strict";
+	var commandSent = document.getElementById("userCommandInput").value;
+	document.getElementById("userCommandInput").value = null;
+	addActivity(commandSent);
+}
+
+
+
+function load() {
+	"use strict";
+	var textEntry = document.getElementById("userCommandInput");
+	textEntry.addEventListener('keydown', keyDown);
+}
+
+function keyDown(event) {
 	"use strict";
 	var command = document.getElementById("userCommandInput"),
 		status = document.getElementById("status");
@@ -7,23 +35,9 @@ function checkCommandInput() {
 	} else {
 		status.innerHTML = "Waiting for send command.";
 	}
-}
-
-function addActivity(command) {
-	"use strict";
-	var receivedCommand = command,
-		currentActivities = document.getElementById("activityLog").innerHTML,
-		newActivityLog = currentActivities + "<br>" + receivedCommand;
-	document.getElementById("activityLog").innerHTML = newActivityLog;
-}
-
-
-function sendCommand() {
-	"use strict";
-	var commandSent = document.getElementById("userCommandInput").value;
-	addActivity(commandSent);
-}
-
-function load() {
-	"use strict";
+	if (event.key === "Enter") {
+		addActivity(command.value);
+		command.value = "";
+		status.innerHTML = "Ready.";
+	}
 }
