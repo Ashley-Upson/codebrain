50288725b2832d5b046c51c00ec4d2eb50cfc412
Ashley Upson <ashmisterupson@gmail.com>
2016-07-14 01:57:25 +0100

    update

--- scripts/game.js
+++ scripts/game.js
@@ -29,19 +29,6 @@ function randomBetween(min, max) {
 	return (Math.floor(Math.random() * max) + 1);
 }
 
-function timer(count) {
-	"use strict";
-	document.getElementById("status").innerHTML = "Executing Command.";
-	var date = new Date(),
-		end = new Date(),
-		seconds = date.getSeconds(),
-		finish = end.setSeconds(seconds + count);
-	while (date <= finish) {
-		date = new Date();
-		console.log("iteration in the timer function");
-	}
-}
-
 function scan(userCommand) {
 	"use strict";
 	var ip1 = randomBetween(1, 255),
@@ -52,11 +39,63 @@ function scan(userCommand) {
 		index = 0;
 	if (userCommand === "scan();") { // Control Server
 		console.log("addActivity called.");
-		addActivity("Scanning for IP addresses.", "console");
 		ipTable[ipTable.length] = ipAddress;
-		console.log("timer called.");
+	} else if (userCommand === "list();") { // Display player botnet
+		timer(1);
+		document.getElementById("status").innerHTML = "Command Executed.";
+		addActivity("Listing IP addresses.", "console");
+		while (index < ipTable.length) {
+			timer(1);
+			addActivity(ipTable[index], "console");
+			index = index + 1;
+		}
+	} else if (userCommand === "firewall();") { // Player firewall
 		timer(5);
 		document.getElementById("status").innerHTML = "Command Executed.";
+		addActivity("Player Firewall.", "console");
+	} else if (userCommand === "anti-virus();") { // Player anti-virus
+		timer(5);
+		document.getElementById("status").innerHTML = "Command Executed.";
+		addActivity("Player Anti-virus.", "console");
+	} else if (userCommand === "decrypt();") { // Player decryption algorithm
+		timer(5);
+		document.getElementById("status").innerHTML = "Command Executed.";
+		addActivity("Player Decryption Algorithm.", "console");
+	} else if (userCommand === "bot();") { // Player bot program
+		timer(5);
+		document.getElementById("status").innerHTML = "Command Executed.";
+		addActivity("Player Bot Program.", "console");
+	} else if (userCommand === "control();") { // Player server
+		timer(5);
+		document.getElementById("status").innerHTML = "Command Executed.";
+		addActivity("Player Control Server.", "console");
+	} else if (userCommand === "botnet();") { // Player botnet efficiency
+		timer(5);
+		document.getElementById("status").innerHTML = "Command Executed.";
+		addActivity("Player Botnet efficiency.", "console");
+	} else if (userCommand === "trojan();") { // Player trojan to attack enemy firewall
+		timer(5);
+		document.getElementById("status").innerHTML = "Command Executed.";
+		addActivity("Firewall Attack Program.", "console");
+	} else if (userCommand === "malware();") { // Player malware to attack enemy anti-virus
+		timer(5);
+		document.getElementById("status").innerHTML = "Command Executed.";
+		addActivity("Anti-virus Attack Program.", "console");
+	} else if (userCommand === "attack_botnet();") { // Player program to attack the enemy botnet and sieze control of it
+		timer(5);
+		document.getElementById("status").innerHTML = "Command Executed.";
+		addActivity("Botnet attack Program.", "console");
+	} else if (userCommand === "hack();") { // Take enemy BrainCoins
+		timer(5);
+		document.getElementById("status").innerHTML = "Command Executed.";
+		addActivity("Wiring enemy BrainCoins to Player Account.", "console");
+	} else {
+		addActivity("Command: '" + userCommand + "' not known. Terminating.", "console");
+	}
+	
+	if (userCommand === "scan();") { // Control Server
+		console.log("timer called.");
+		timer(5, "Scanning for IP addresses.", "console");
 		addActivity("Found an IP address: " + ipAddress, "console");
 	} else if (userCommand === "list();") { // Display player botnet
 		timer(1);
@@ -138,6 +177,20 @@ function keyDown(event) {
 	}
 /*	addActivity("","user");
 	document.getElementById("activityLog").innerHTML = activityLog + event.key;*/
+}
+
+function timer(count, message, type) {
+	"use strict";
+	addActivity(message, type);
+	document.getElementById("status").innerHTML = "Executing Command.";
+	var date = new Date(),
+		end = new Date(),
+		seconds = date.getSeconds(),
+		finish = end.setSeconds(seconds + count);
+	while (date <= finish) {
+		date = new Date();
+		console.log("iteration in the timer function");
+	}
 }
 
 function load() {
