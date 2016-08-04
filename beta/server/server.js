d362b2d4de4b866fa71a9f1677a01bff57807a8b
Ashley Upson <ashmisterupson@gmail.com>
2016-08-04 04:02:40 +0100

    Updated documentation of 'beta/server/server.js'.

--- beta/server/server.js
+++ beta/server/server.js
@@ -1,129 +1,91 @@
-/*
-	--------
-	OLD CODE
-	--------
-
-var express = require("express"),// Require "express".
-	fs = require("fs"),// Require "fs".
-	app = express(),// Define the application variable.
-	ip = process.env.OPENSHIFT_NODEJS_IP,// Define the IP address as either the OpenShift provided one or localhost.
-	port = process.env.OPENSHIFT_NODEJS_PORT;// Define the port as either the OpenShift provided one or 8080.
-
-if (process.env.OPENSHIFT_NODEJS_PORT === null || process.env.OPENSHIFT_NODEJS_PORT === undefined) {
-	port = 8080;
+var express = require('express'),// Require the "express" module
+	app = express(),// Create the handle for the "express" module
+	expressWs = require('express-ws')(app),// Require the "express-ws" module
+	dataToSend,// Variable to hold the data that is to be sent to the client
+	clientToRecieve;// Variable to hold the client ID for the response
+/**
+ *	---------------------------------------
+ *	INSERT CODE FOR ALL GAME FUNCTIONS HERE
+ *	---------------------------------------
+ */
+function randomBetween(min, max) {// Function to generate a random number between min and max inclusive
+	"use strict";// Check the code against established coding standards
+	return (Math.floor(Math.random() * max) + 1);// Generate and return the random number
 }
-
-app.get("/", function (request, response) {
-	response.send("hello world");
-});
-
-if (process.env.OPENSHIFT_NODEJS_IP === null || process.env.OPENSHIFT_NODEJS_IP === undefined) {
-	ip = "127.0.0.1";
-	app.listen(port);
-} else {
-	app.listen(ip, port);
-}
-
-console.log(ip + ":" + port);
-
-*/
-
-var express = require('express'),
-	app = express(),
-	expressWs = require('express-ws')(app),
-	dataToSend;
- 
-/*
-	---------------------------------------
-	INSERT CODE FOR ALL GAME FUNCTIONS HERE
-	---------------------------------------
-*/
-
-function randomBetween(min, max) {
-	"use strict";
-	return (Math.floor(Math.random() * max) + 1);
-}
-
-function gameCommands(command) {// Called by parseWebSocketData - deals with commands sent by the client.
-	if(command === "scan"){
-		pcScan();
-	} else if(command === "") {
-				
+function gameCommands(command) {// Function to deal with commands sent by the client - called by ParseWebSocketData
+	if(command === "scan"){// If the command recieved is "scan"
+		pcScan();// Call the function relevant to the client command
+	} else if(command === "") {// If the command recieved is ""
+		//Call the fucntion relevant to the client command 
 	}
 }
-
 // All the fucnctions that are to be used by the player.
-function pcScan(){// Server logic for the player input of "scan();".
-	"use strict";
-	var ip1 = randomBetween(1, 255),
-		ip2 = randomBetween(1, 255),
-		ip3 = randomBetween(1, 255),
-		ip4 = randomBetween(1, 255),
-		ipAddress = ip1.toString() + "." + ip2.toString() + "." + ip3.toString() + "." + ip4.toString();
-	// This was just "ws" at one point
-	//expressWs.send("pcScan;" + ipAddress);
-	dataToSend = "pcScan;" + ipAddress;
+function pcScan(client){// Server logic for the player input of "scan();".
+	"use strict";// Check the code against established coding standards
+	var ip1 = randomBetween(1, 255),// Generate the first section of the IP address
+		ip2 = randomBetween(1, 255),// Generate the second section of the IP address
+		ip3 = randomBetween(1, 255),.// Generate the third section of the IP address
+		ip4 = randomBetween(1, 255),// Generate the fourth section of the IP address
+		ipAddress = ip1.toString() + "." + ip2.toString() + "." + ip3.toString() + "." + ip4.toString();// Create the whole generated IP address
+	dataToSend = "pcScan;" + ipAddress;// Set the data to be sent to the client
+	clientToRecieve = client;// Set the client to recieve the data
 }
-
 function pcList() {// Server logic for the player input of "list();".
 	
 }
-
 function pcFirewall() {// Server logic for the player input of "firewall();".
 	
 }
-
 function pcAV() {// Server logic for the player input of "antivirus();".
 	
 }
-
 function pcDecrypt() {// Server logic for the player input of "decrypt();".
 	
 }
-
 function pcBot() {// Server logic for the player input of "bot();".
 	
 }
-
 function pcBotnet() {// Server logic for the player input of "botnet();".
 	
 }
-
 function pcAttackBotnet() {// Server logic for the player input of "attack_botnet();".
 	
 }
-
 function pcControl() {// Server logic for the player input of "control();".
 	
 }
-
 function pcTrojan() {// Server logic for the player input of "trojan();".
 	
 }
-
 function pcMalware() {// Server logic for the player input of "malware();".
 	
 }
-
 function pcHack() {// Server logic for the player input of "hack();".
 	
 }
-
-function parseWebSocketData(receivedData) {// Deals with ALL data sent from the client.
-	// Data structure of all data sent to the server:
-	// {"primaryType;secondaryType;optionsalData"}
-	/*
-		primaryType:
-			command
-			serverData
-			
-		secondaryType:
-			if primaryType === command
-				addActivity;
-				pcScan;
-				pcList;
-				pcFirewall;
-	*/
+function parseWebSocketData(receivedData, client) {// Deals with ALL data recieved from the client.
+	/**
+	 *	Data structure of all data sent to the server:
+	 *	{"primaryType;secondaryType;optionalData"}
+	 *		primaryType:
+	 *			command
+	 *			serverData
+	 *		secondaryType:
+	 *			if primaryType === command
+	 *				addActivity;
+	 *				pcScan;
+	 *				pcList;
+	 *				pcFirewall;
+	 *				pcAV;
+	 *				pcDecrypt;
+	 *				pcBot;
+	 *				pcBotnet;
+	 *				pcAttackBotnet;
+	 *				pcControl;
+	 *				pcTrojan;
+	 *				pcMalware;
+	 *				pcHack;
+	 */
 	console.log(receivedData);
 	var firstSemiColon = receivedData.indexOf(";"),
 		primaryType = receivedData.substring(0,firstSemiColon),
@@ -132,35 +94,27 @@ function parseWebSocketData(receivedData) {// Deals with ALL data sent from the 
 		secondaryType = currentData,
 		all = primaryType + " ... " + secondaryType;
 	console.log(secondaryType);
-	console.log(aWss.clients)
-	//Won't work. As you can clearly see in the GitHub code they defined aWss.
-	//Oh yeah...
-	
+	console.log(aWss.clients);
 	if(primaryType === "command"){
 		if(secondaryType === "pcScan"){
-			pcScan();
+			pcScan(client);
 		}
 	}
 }
-
 /*  ---------------------------------------  */
-
 app.use(function (request, response, next) {
 	request.testing = 'server';
 	return next();
 });
- 
 app.get('/', function(request, response, next){
 	console.log('get route', request.testing);
 	response.end();
 });
- 
-/*
-  -------------------------------------------------------
-  |	This function deals with data sent from the client. |
-  -------------------------------------------------------
-*/
-
+/**
+ *	-------------------------------------------------------
+ *	| This function deals with data sent from the client. |
+ *	-------------------------------------------------------
+ */
 app.ws('/', function(ws, req) {
     var clientSocket = req.socket;
 	ws.on('message', function(msg) {
@@ -180,6 +134,5 @@ setInterval(function () {
 		}
 	});
 }, 5000);
-
 app.listen(8080);
 console.log("Executed all code.");
